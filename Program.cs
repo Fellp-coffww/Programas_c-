2eab325b18fa4fd6a53ea6c7498b5c27c55873a3
Fellp <fellipe.augusto01@gmail.com>
2022-11-03 17:19:13 -0300

    nova classe

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace main2{
     class Program{
     static void Main(string[] args){
 
-       
+       morgana m = new morgana();
+       double p = m.x1 = 10;
+       System.Console.WriteLine(p);
     }
 
 }}
---
+++ morgana.cs
@@ -0,0 +1,7 @@
+    public class morgana
+    {
+        public double x1;
+       public double x2;
+       public double x3;
+       public double x4;
+    }
